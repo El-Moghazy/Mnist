c2d8621e3e67124f84bbbb547e6338582087df82
ElMoghazy <abdelrhman.almoghazy@student.guc.edu.eg>
2017-07-29 11:36:47 +0200

    Changed

--- mnist.py
+++ mnist.py
@@ -6,6 +6,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import Flatten
+from keras.layers.core import Activation
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
@@ -18,20 +19,20 @@ numpy.random.seed(seed)
 # load data
 test = pd.read_csv('C:/Users/ElMoghazy/Desktop/test.csv')
 training = pd.read_csv('C:/Users/ElMoghazy/Desktop/train.csv')
-X_test = test
-train = ["pixel" +str(x) for x in range(784)]
+X_test2 = test
+train = ["pixel" + str(x) for x in range(784)]
 X_train = training[train]
 y_train = training['label']
-X_test = X_test.as_matrix()
+X_test2 = X_test2.as_matrix()
 X_train = X_train.as_matrix()
 y_train = y_train.as_matrix()
 
 # reshape to be [samples][pixels][width][height]
 X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+X_test2 = X_test2.reshape(X_test2.shape[0], 1, 28, 28).astype('float32')
 # normalize inputs from 0-255 to 0-1
 X_train = X_train / 255
-X_test = X_test / 255
+X_test2 = X_test2 / 255
 # one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 num_classes = 10
@@ -41,24 +42,22 @@ num_classes = 10
 def larger_model():
     # create model
     model = Sequential()
-    model.add(Conv2D(96, (11, 11), input_shape=(1, 28, 28), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3 ,3)))
-    model.add(Conv2D(256, (5, 5), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3 ,3)))
-    model.add(Conv2D(384, (3, 3), activation='relu'))
-    model.add(Conv2D(384, (3, 3), activation='relu'))
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-
-
+    # CONV => RELU => POOL
+    model.add(Conv2D(20, kernel_size=5, padding="same",input_shape=(1, 28, 28)))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # CONV => RELU => POOL
+    model.add(Conv2D(50, kernel_size=5, padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # Flatten => RELU layers
     model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1000, activation='relu'))
+    model.add(Dense(500))
+    model.add(Activation("relu"))
 
-    model.add(Dense(num_classes, activation='softmax'))
-    # Compile model
+    # a softmax classifier
+    model.add(Dense(10))
+    model.add(Activation("softmax"))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
@@ -66,12 +65,12 @@ def larger_model():
 # build the model
 model = larger_model()
 # Fit the model
-model.fit(X_train, y_train, epochs=15, batch_size=200)
+model.fit(X_train, y_train, epochs=5, batch_size=200)
 # Final evaluation of the model
 # scores = model.evaluate(X_test, y_test, verbose=0)
 # print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
-result = model.predict(X_test)
-result = numpy.argmax(result,axis = 1)
-result = pd.Series(result,name="Label")
-submission = pd.concat([pd.Series(range(1,28001),name = "ImageId"),result],axis = 1)
-submission.to_csv("C:/Users/ElMoghazy/Desktop/f.csv",index=False)
+result = model.predict(X_test2)
+result = numpy.argmax(result, axis=1)
+result = pd.Series(result, name="Label")
+submission = pd.concat([pd.Series(range(1, 28001), name="ImageId"), result], axis=1)
+submission.to_csv("C:/Users/ElMoghazy/Desktop/f.csv", index=False)
